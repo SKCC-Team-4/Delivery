0b38b3799b779a4147ac5c6a6b2434ca2ea676ba
zerotosilver <zerosilvertwo@gmail.com>
2020-04-21 10:24:50 +0900

    first commit

---
+++ src/main/java/BookStore/PolicyHandler.java
@@ -0,0 +1,29 @@
+package BookStore;
+
+import BookStore.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverPayApproved_DeliveryRequest(@Payload PayApproved payApproved){
+
+        if(payApproved.isMe()){
+            System.out.println("##### listener DeliveryRequest : " + payApproved.toJson());
+        }
+    }
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverPayCanceled_DeliveryCancel(@Payload PayCanceled payCanceled){
+
+        if(payCanceled.isMe()){
+            System.out.println("##### listener DeliveryCancel : " + payCanceled.toJson());
+        }
+    }
+
+}
