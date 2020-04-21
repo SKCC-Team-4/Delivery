5a2b342a27fea0699fd5c9be7beee700683f51a6
zerotosilver <zerosilvertwo@gmail.com>
2020-04-21 16:25:47 +0900

    Merge branch 'master' of https://github.com/SKCC-Team-4/Delivery
    # Conflicts:
    #	README.md
    #	src/main/java/BookStore/PolicyHandler.java

--- src/main/java/BookStore/PolicyHandler.java
+++ src/main/java/BookStore/PolicyHandler.java
@@ -8,30 +8,26 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PolicyHandler{
-<<<<<<< HEAD
 
     @Autowired
     DeliveryRepository deliveryRepository;
-=======
->>>>>>> 87a68d5fd7eba1a9d55cd2b622e6a183be6130ca
     
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverPayApproved_DeliveryRequest(@Payload PayApproved payApproved){
 
         if(payApproved.isMe()){
-<<<<<<< HEAD
             System.out.println("111##### listener DeliveryRequest : " + payApproved.toJson());
         }
 
-        deliveryRepository.findById(payApproved.getOrderId());
-        System.out.println("222##### listener DeliveryRequest : " + payApproved.toJson());
-=======
-            System.out.println("##### listener DeliveryRequest : " + payApproved.toJson());
-        }
->>>>>>> 87a68d5fd7eba1a9d55cd2b622e6a183be6130ca
+        Optional<Delivery> opt = deliveryRepository.findById(payApproved.getOrderId());
+        Delivery delivery = opt.get();
+        System.out.println("222##### listener DeliveryRequest : " + delivery.getStatus());
     }
+
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverPayCanceled_DeliveryCancel(@Payload PayCanceled payCanceled){
 
